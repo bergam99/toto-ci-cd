76f6b61c84594fbed8b5de16538b735895d72f49
Jiyun <grosjeanjiyun@icloud.com>
2025-11-03 14:39:51 +0100

    init

---
+++ toto-presentation/src/views/AboutView.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      locale: import.meta.env.VITE_I18N,
+      url: import.meta.env.VITE_BACKEND_URL,
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>{{ locale }}</p>
+    <p>{{ url }}</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
