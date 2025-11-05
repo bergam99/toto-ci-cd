dc8ef7384046688fad15fb138443e645d1f87950
Jiyun <grosjeanjiyun@icloud.com>
2025-11-05 12:04:01 +0100

    presentation utl

--- toto-jenkins/prepare-presentation.sh
+++ toto-jenkins/prepare-presentation.sh
@@ -5,6 +5,7 @@
 readonly SCRIPT=$0
 readonly ENV_FILE_PATH=$1
 readonly TOTO_PRESENTATION_DEPLOY_DIR=$2
+readonly TOTO_PRESENTATION_URL=$3
 
 echo "> Execution ${SCRIPT}"
 
@@ -28,3 +29,6 @@ npm --prefix ./toto-presentation/ run build
 echo " > Deploy dist/* to ${TOTO_PRESENTATION_DEPLOY_DIR}"
 rm -r $TOTO_PRESENTATION_DEPLOY_DIR/* # supprimer dans le folder
 cp -r ./toto-presentation/dist/* $TOTO_PRESENTATION_DEPLOY_DIR
+
+echo " > Deployed !!"
+echo $TOTO_PRESENTATION_URL
