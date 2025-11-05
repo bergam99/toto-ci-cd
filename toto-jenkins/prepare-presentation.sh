e17d8c31ce6cfba59e4a03cc9781f4b7c84b3899
Jiyun <grosjeanjiyun@icloud.com>
2025-11-05 11:55:04 +0100

    add rm cp dist

--- toto-jenkins/prepare-presentation.sh
+++ toto-jenkins/prepare-presentation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-
 # $0 = script meme
 # declarer constant
 readonly SCRIPT=$0
 readonly ENV_FILE_PATH=$1
+readonly TOTO_PRESENTATION_DEPLOY_DIR=$2
 
 echo "> Execution ${SCRIPT}"
 
@@ -20,6 +20,11 @@ echo " > Generate artifacts with npm: "
 npm --prefix ./toto-presentation/ install # faire npm install dans le dossier toto-presentation mais pas dnas le workspace racine.
 
 # .env.production 
+echo " > Copy tmp env file to ./toto-presentation/.env.production"
 cp $ENV_FILE_PATH ./toto-presentation/.env.production # copie source -> destination
 
 npm --prefix ./toto-presentation/ run build
+
+echo " > Deploy dist/* to ${TOTO_PRESENTATION_DEPLOY_DIR}"
+rm -r $TOTO_PRESENTATION_DEPLOY_DIR/* # supprimer dans le folder
+cp -r ./toto-presentation/dist/* $TOTO_PRESENTATION_DEPLOY_DIR
