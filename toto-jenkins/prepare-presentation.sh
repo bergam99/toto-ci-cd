8a6686f823f415b56021d59e7fe49f7d7c1a9f0d
Jiyun <grosjeanjiyun@icloud.com>
2025-11-05 10:26:56 +0100

    add generated artifacts

--- toto-jenkins/prepare-presentation.sh
+++ toto-jenkins/prepare-presentation.sh
@@ -3,3 +3,10 @@
 echo " > Preparing presentation artifacts with node and npm: "
 node -v
 npm -v
+
+echo " > Generate artifacts with npm: "
+
+# => toto-presentation/ 
+
+npm --prefix ./toto-presentation/ install # faire npm install dans le dossier toto-presentation mais pas dnas le workspace racine.
+npm --prefix ./toto-presentation/ run build
