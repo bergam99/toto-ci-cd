1e8ba2b54fc8481f8131aba7613fc63325828682
Jiyun <grosjeanjiyun@icloud.com>
2025-11-06 13:12:44 +0100

    build back

---
+++ toto-jenkins/prepare-buisness.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e # Failure et arreter à l'erreur
+
+# $0 = script meme
+# declarer constant
+readonly SCRIPT=$0
+readonly ENV_FILE_PATH=$1
+
+echo "> Execution ${SCRIPT}"
+
+echo " > Preparing buisness artifacts with maven and java: "
+
+mvn -v
+mvn -Dmaven.test.skip=true -f ./toto-buisness/toto-buisness.pom.xml package
+
+echo " > Build completed succesfully"
