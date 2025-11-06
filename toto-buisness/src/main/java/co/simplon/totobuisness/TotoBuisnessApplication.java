755b176530ab2b9475049e6feab46b81af1d54a1
Jiyun <grosjeanjiyun@icloud.com>
2025-11-06 16:30:47 +0100

    force rm, /ping

--- toto-buisness/src/main/java/co/simplon/totobuisness/TotoBuisnessApplication.java
+++ toto-buisness/src/main/java/co/simplon/totobuisness/TotoBuisnessApplication.java
@@ -1,12 +1,20 @@
 package co.simplon.totobuisness;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class TotoBuisnessApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TotoBuisnessApplication.class, args);
+	}
+
+	@GetMapping("/ping")
+	String ping(){
+		return "pong";
 	}
 }
