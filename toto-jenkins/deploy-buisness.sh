2316519548e54f908a6ccdfafb1d67f5b4561c30
Jiyun <grosjeanjiyun@icloud.com>
2025-11-07 10:37:54 +0100

    add stop app

--- toto-jenkins/deploy-buisness.sh
+++ toto-jenkins/deploy-buisness.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-set -e # Failure et arreter à l'erreur
+# set -e # Failure et arreter à l'erreur
 
 readonly SCRIPT=$0
 readonly TOTO_BUISNESS_DEPLOY_DIR=$1 # deposer .jar
 readonly TOTO_BUISNESS_URL=$2
+readonly TOTO_BUISNESS_PORT=$3
 
 echo "> Execution ${SCRIPT}"
+
+echo "> Stopping toto-buisness (optional operation)"
+readonly PID=$(lsof -ti tcp:$TOTO_BUISNESS_PORT) # recuperer le port en variable au niveau propriete folder
+readonly PID_EXIT_CODE=$?
+
+set -e # pas tout en haut si pid exists pas
+
+if [$PID_EXIT_CODE -eq 0]; then
+    # Stop app
+    echo "killing gracefully the process with PID=${PID}..."
+    kill -15 $PID
+    echo "Process killed"
+
+elif
+    echo "process isn't running"
+fi
+
 
 echo " > Deploy target/*.jar to ${TOTO_BUISNESS_DEPLOY_DIR}"
 rm -rf $TOTO_BUISNESS_DEPLOY_DIR/* # supprimer dans le folder l'ancien build
